271caef74a477dca4fbae8915b2dde8359d606be
ahmaderta rizqi <ahmadderta.rizqi30@gmail.com>
2020-05-23 13:17:12 +0700

    add .env variables

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
+export const apiKEY = process.env.MOVIE_API_KEY;
 export const apiURL = "https://api.themoviedb.org/3";
-export const apiKEY = "6414490597c2c3e4d613172e288a702e";
 export const imageURL = "https://image.tmdb.org/t/p";
 
 export const iconSettings = {
