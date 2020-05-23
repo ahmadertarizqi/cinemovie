b2a9159570f32f946dc22d0badc4e3e706132e8b
ahmaderta rizqi <ahmadderta.rizqi30@gmail.com>
2020-05-23 12:25:48 +0700

    fix some problem in webpack configuration

--- webpack.common.js
+++ webpack.common.js
@@ -34,15 +34,15 @@ module.exports = {
          {
             test: /\.(png|svg|jpg|gif)$/,
             use: [
-              'file-loader',
+               'file-loader',
             ],
-          },
-          {
+         },
+         {
             test: /\.(woff|woff2|eot|ttf|otf)$/,
             use: [
-              'file-loader',
+               'file-loader',
             ],
-          },
+         },
       ]
    },
    plugins: [
